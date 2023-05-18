009788f41428377fd0000dd17b58dfc09ed5ec8a
shermondevelopment <victorshermon@yahoo.com>
2023-05-18 15:24:02 -0300

    feat: creating connection with mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [],
-  providers: [],
+  providers: []
 })
 export class AppModule {}

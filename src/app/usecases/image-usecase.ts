574908f71c51a497654cc0bd6689f3818213a397
shermondevelopment <victorshermon@yahoo.com>
2023-05-18 18:33:10 -0300

    feat: saving original image in locally

--- src/app/usecases/image-usecase.ts
+++ src/app/usecases/image-usecase.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common'
 import { ImageTransformProperty, TransformImage } from '../domain/image'
 import axios from 'axios'
+import * as fs from 'fs'
+import * as path from 'path'
 
 @Injectable()
 export class TransformImageUseCase implements TransformImage {
@@ -8,7 +10,13 @@ export class TransformImageUseCase implements TransformImage {
     const { image, compress } = imageOptions
 
     const getImage = await axios.get(image, { responseType: 'arraybuffer' })
+    const imageToBuffer = Buffer.from(getImage.data)
 
-    console.log(getImage)
+    const pathImageToSaved = path.join(__dirname, '../../../uploads')
+
+    const originalPath = path.join(pathImageToSaved, 'original.jpg')
+
+    /* saving original image */
+    fs.writeFileSync(originalPath, imageToBuffer)
   }
 }

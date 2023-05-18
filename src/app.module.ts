b66ec49b623e37f9d12078ddce493b0fd23104bb
shermondevelopment <victorshermon@yahoo.com>
2023-05-18 16:49:08 -0300

    feat: creating a docker compose with a mongo database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env'
+    }),
+    MongooseModule.forRoot('mongodb://localhost:27017/trakto')
+  ],
   controllers: [],
   providers: []
 })

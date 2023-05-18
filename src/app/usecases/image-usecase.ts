587876abc86f94ffefaf18980ae25a57225fbf52
shermondevelopment <victorshermon@yahoo.com>
2023-05-18 18:46:05 -0300

    feat: save image thumbnail in locally

--- src/app/usecases/image-usecase.ts
+++ src/app/usecases/image-usecase.ts
@@ -3,6 +3,7 @@ import { ImageTransformProperty, TransformImage } from '../domain/image'
 import axios from 'axios'
 import * as fs from 'fs'
 import * as path from 'path'
+import * as sharp from 'sharp'
 
 @Injectable()
 export class TransformImageUseCase implements TransformImage {
@@ -15,8 +16,14 @@ export class TransformImageUseCase implements TransformImage {
     const pathImageToSaved = path.join(__dirname, '../../../uploads')
 
     const originalPath = path.join(pathImageToSaved, 'original.jpg')
+    const thumbnailPath = path.join(pathImageToSaved, 'thumb.jpg')
 
     /* saving original image */
     fs.writeFileSync(originalPath, imageToBuffer)
+
+    /* save image thumbnail */
+    await sharp(imageToBuffer)
+      .resize(720, 720, { fit: 'inside' })
+      .toFile(thumbnailPath)
   }
 }
